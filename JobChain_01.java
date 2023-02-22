414a288e90b174bc5e3223be91d575a2cb3ea57e
jyoti <jyoti.khaire1992@gmail.com>
2023-02-22 08:05:14 +0530

    commiting job chaining

--- JobChain_01.java
+++ JobChain_01.java
@@ -1,8 +1,10 @@
-public class HelloWorld {
+//package demo;
+
+public class JobChain_01 {
 
 	public static void main(String[] args) {
-
-		System.out.println("This is parent job chaining ");
+	
+		System.out.println("This is Parent Job chaining");
 	}
 
 }
--- JobChain_02.java
+++ JobChain_02.java
@@ -1,8 +1,9 @@
-public class HelloWorld {
+//package demo;
+
+public class JobChain_02 {
 
 	public static void main(String[] args) {
-
-		System.out.println("This is child job chaining ");
+		System.out.println("This is child job chaining");
 	}
 
 }
